3a296e21634aa8198de41904094c6783933ca171
jiteshsoni1 <jiteshsoni3591@gmail.com>
2026-01-17 14:01:20 +0530

    sidebar

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,10 +1,25 @@
 import "./Sidebar.css";
 
-function Sidebar(){
-    return(
-        <div>
-            Sidebar
-        </div>
-    )
+function Sidebar() {
+  return (
+    <section className="sidebar">
+      <button>
+        <img src="src\assets\blacklogo.png" alt="gpt logo" className="logo"></img>
+        <i className="fas fa-edit"></i>
+      </button>
+
+      <ul className="history">
+        <li>thread1</li>
+        <li>thread2</li>
+        <li>thread3</li>
+      </ul>
+      <div className="sign">
+        <span><p>By Apnacollege &hearts;</p></span>
+        
+    
+     
+      </div>
+    </section>
+  );
 }
 export default Sidebar;
